b9e24b49798068f72b496832620b233f58c076ce
praveen <praveenpalli332@gmail.com>
2023-10-13 17:47:47 +0530

    add test files

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@ am writing about deploy
 forr my learning proces
 how are you
 
-i am updating this code this time master....
-
-i am updating this code....
-now am updated dev branch
+i am updating..............
 
 
